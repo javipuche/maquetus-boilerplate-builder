a2beb742fad48503d4951859a577c92496bb0284
Javi Puche <javierpuchevictoria@gmail.com>
2019-08-01 08:37:48 +0200

    refactor: Eliminados espacios en blanco de más en el preview del componente

--- docs/compileComponentPreview.js
+++ docs/compileComponentPreview.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const hbs = require('handlebars')
 const getComponentPreviewData = require('./getComponentPreviewData')
+const trimQuotes = require('../utils/trimQuotes')
 
 const compileComponentPreview = (file, data) => {
     data = data || getComponentPreviewData(file)
     const content = fs.readFileSync(file, 'utf8')
     const template = hbs.compile(content)
-    return template(data)
+    return trimQuotes(template(data))
 }
 
 module.exports = compileComponentPreview
